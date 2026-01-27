3aa2adb00c599d8a1066384958d4c74d1bb0d4d0
Quincybob <74205675+Quincybob@users.noreply.github.com>
2026-01-27 16:43:20 +0300

    fix callback issues

--- components/tabs/SwapTab.tsx
+++ components/tabs/SwapTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useEffect, useRef, useMemo, useCallback } from "react";
 import SwapInterface from "../swap/SwapInterface";
 import { useInflationData } from "../../hooks/use-inflation-data";
 import type { Region } from "../../hooks/use-user-region";
